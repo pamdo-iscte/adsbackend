33b410ac67aa5771b0925ef7d9f5740883210dfd
pamdo-iscte <pamdo@iscte-iul.pt>
2022-11-01 12:25:42 +0000

    Spring Project

---
+++ src/main/java/com/backendads/backendads/BackendAdsApplication.java
@@ -0,0 +1,13 @@
+package com.backendads.backendads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backendads/backendads/BackendAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendads.backendads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
