a53baa8dea6f9c930e41212820af2021bf2ba68e
indco-iscte <99822984+indco-iscte@users.noreply.github.com>
2022-12-21 15:02:47 +0000

    Create Turno.java

---
+++ src/main/java/Files/Turno.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.List;
+
+public class Turno {
+
+	List<String> turmas;
+	List<String> cursos;
+	String unidades_execucao;
+	int inscritos;
+	String turno;
+	
+	
+	
+}
